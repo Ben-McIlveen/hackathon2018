6249c4a8411d9006366350f1ef22f661d3919eac
macapakaz <benmcilveen.benacy@gmail.com>
2018-02-24 14:16:00 +0000

    Update register.py

--- register/register.py
+++ register/register.py
@@ -1,1 +1,26 @@
+from discord.ext import commands
+from discord import Embed
+from .utils import checks
+from .utils.dataIO import dataIO
+import os
 
+class register():
+    def __init__(self, bot):
+      self.bot=bot
+
+def check_folders(): #Creates a folder
+    if not os.path.exists("data/"):
+        print("Creating data/hackathon")
+        os.makedirs("data/hackathon")
+
+def check_files(): #Creates json files in the folder
+    if not dataIO.is_valid_json("data/hackathon/settings.json"):
+        print("Creating empty settings.json...")    
+        dataIO.save_json("data/hackathon/settings.json", {})
+  
+def setup(bot):
+    check_folders()
+    check_files()
+    n = register(bot)
+    bot.add_listener(n.server_join, "on_server_join")
+    bot.add_cog(n)
