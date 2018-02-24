cdc8322152cfedb4353e42f4eaa7e790eeb97b76
eaten-squid <36794665+eaten-squid@users.noreply.github.com>
2018-02-24 21:04:40 +0000

    Added pymongo func
    Added more features... Most *should work*
    Added pymongo functionality

--- register/register.py
+++ register/register.py
@@ -4,38 +4,102 @@ from .utils import checks
 from .utils.dataIO import dataIO
 import os
 
+try:
+    import pymongo
+    from pymongo import MongoClient
+    client =  MongoClient()
+    db = client["challange-channels"]
+except:
+    raise RuntimeError("Could not load Database - Check the GIT docs for more info.")
+
 class register():
     def __init__(self, bot):
-        self.bot=bot
+        self.bot=bot  
         
-        
-    async def register(self,user):
+    async def register(self,user, server=None):
+        if server == None:
+            server = user.server
+
+        Details = {}
+        roles = ["Filler"]
+        Details["USER_ID"]=user.id
+
+        Registed_role = await self.get_role(server, "registed")
+        if Registed_role is None:
+            Registed_role = await self.make_role(server, "registed")
+
         prv_channel = await self.bot.start_private_message(user)
-        await self.bot.send_message(prv_channel, "Welcome to {user.server}! Please tell us what company you work for - ".format(user))
-        msg = await self.bot.wait_for_message(timeout=120, channel = prv_channel, author = user).lower()
-        role.append(await self.get_role(user.server, msg.content))
+        await self.bot.send_message(prv_channel, "Welcome to {0}! Please tell us what company you work for - ".format(server))
+        msg = await self.bot.wait_for_message(timeout=120, channel = prv_channel, author = user)
+
+        if msg is not None:
+            Details["COMPANY"] = msg.content.lower()
+            role = await self.get_role(server, msg.content.lower())
+            if role is None:
+                role = await self.make_role(server, msg.content)
+            await self.bot.add_roles(user, role)
+
+        else:
+            Details["COMPANY"] = None
         
-        if role[0] is None:
-            role[0] = await self.make_role(user.server, msg.content).lower()
+        await self.bot.send_message(prv_channel, "What skills/job role do you have? (type `exit` to finish)")
+        while roles[-1] is not None:
+            msg = await self.bot.wait_for_message(timeout=60, channel = prv_channel, author = user)
+            if msg is None:
+                roles.append(None)
+
+            elif msg.content.lower() != "exit": roles.append(msg.content.lower())
+
+            else: roles.append(None)
+
+        if len(roles)<3:
+            Details["ROLES"] = roles[:-2]
         
-        await self.bot.send_message(prv_channel, "What skills do you have? (type `exit` to finish)")
-        while role[:-1] is not None:
-            msg = await self.bot.wait_for_message(timeout=120, channel = prv_channel, author = user).lower()
-            if msg.content != "exit" or msg.content is not None:
-                role.append(msg.content)
-            else: role.append(None)
-        
-        for x in role[0:-2]:
-            role_to_add = await self.get_role(user.server, x)
-            if role_to_add is None:
-                await self.make_role(user.server, x)
-        
-    async def get_role(self,server,role):
-        roles = set(ctx.message.server.roles)
+        if len(roles) < 3 or Details["COMPANY"]==None:
+            await self.bot.send_message(prv_channel, "You have not been registered! Ask a mod to be registed")
+
+        #Add max number of roles
+        #Maybe add somekind of "verify role"
+        #Maybe check agaisnt a list of "bad roles" (or just leave it to the mods XD)
+
+        db.users.insert_one(Details)
+        await self.bot.send_message(prv_channel, "You have now been registered!")
+        await self.bot.add_roles(user, Registed_role)
+    
+    async def get_role(self,server,role_name):
+        roles = set(server.roles)
         for role in roles:
-            if role.id == role_id:
+            if role.name == role_name:
                 return role
         return None
+
+    async def make_role(self,server,role_name):
+        return await self.bot.create_role(server,name=role_name)
+    
+    @commands.command(pass_context=True, no_pm=True)
+    async def registerRole(self,ctx,role):
+        pass
+
+    @commands.group(pass_context=True)
+    @checks.serverowner_or_permissions(manage_channels=True)
+    async def setregister(self,ctx):
+        """Changes the settings for this cog, use with no sub command to get infomation on the cog, and current setings"""
+
+    @setregister.command(pass_context=True)
+    @checks.serverowner_or_permissions(manage_roles=True)
+    async def reset(self,ctx, user: discord.Member):
+        """Resets the members roles & settings"""
+        await self.bot.send_message(user, "Your registration has been reset")
+        await self.bot.say("Roles reset")
+        db.users.update_one({"USER_ID": user.id},{'$set': {}}, upsert=False)
+        await self.bot.remove_roles(user, self.get_role(server,"registed"))
+        await self.bot.remove_roles(user, self.get_role(server, db.users.find_one({"USER_ID":user.id})["COMPANY"]))
+
+    @setregister.command(pass_context=True)
+    @checks.serverowner_or_permissions(manage_roles=True)
+    async def forcereg(self, ctx, user:discord.Member):
+        """Forces a user to register"""
+        await self.register(user,ctx.server)
 
 def check_folders(): #Creates a folder
     if not os.path.exists("data/hackathon"):
