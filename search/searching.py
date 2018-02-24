6c14a9aca997339d127d8040dfad11cab03d3a25
macapakaz <benmcilveen.benacy@gmail.com>
2018-02-24 15:04:08 +0000

    Update searching.py

--- search/searching.py
+++ search/searching.py
@@ -1,1 +1,10 @@
+from discord.ext import commands
+from discord import Embed
+from .utils import checks
+from .utils.dataIO import dataIO
+import os
 
+class search():
+  def __init__(self.bot):
+    self.bot = bot
+  @commands.command(pass_context = true)
