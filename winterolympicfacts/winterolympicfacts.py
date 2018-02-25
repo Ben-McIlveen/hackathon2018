93d6c8a2d8efa9887ae8a1d91b0e2edb08bcd83f
ZacPatterson <36793796+ZacPatterson@users.noreply.github.com>
2018-02-25 12:02:32 +0000

    Update winterolympicfacts.py

--- winterolympicfacts/winterolympicfacts.py
+++ winterolympicfacts/winterolympicfacts.py
@@ -25,9 +25,19 @@ async def userinfo(ctx, user: discord.Member):
   embeded.add_field(name="Highest Role", value=user.role_top, inline=True)
   embeded.add_field(name="Lowest Role",value=user.role_bottom,inline=True)
   embeded.add_field(name="Joined",value=user.joined_at,inline=True)
-  embeded.set_thumbnail(url=user_avatar)
-  embeded = embeded
-  await.say(embeded)
+  embeded.set_thumbnail(url=user.avatar_url)
+  await self.say(embeded= embeded)
+  
+@self.command(pass_context=True)
+async def severinfo(ctx)
+embeded=discord.Embeded(title="Server {}'s info".format(server.message.name), description="Here's what I found!", color=0xff8d3d)
+embeded.add_field(name="Server Name:", value = ctx.message.server.name, inline=True)
+embeded.add_field(name="Server ID:", value = ctx.message.server.id, inline = True)
+embeded.add_field(name="Roles:", value = ctx.len(message.server.roles), inline=True)
+embeded.add_field(name="Members:", value = ctx.len(message.server.members), inline=True)
+embeded.set_thumbnail(url=ctx.server.icon_url)
+await self.say(embeded= embeded)
+
   
   
   
