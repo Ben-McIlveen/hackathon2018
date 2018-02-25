8dfc5420a6acf89a972bfda5dbab67195b1b9855
ZacPatterson <36793796+ZacPatterson@users.noreply.github.com>
2018-02-25 13:40:41 +0000

    Update stuff.py

--- winterolympicfacts/stuff.py
+++ winterolympicfacts/stuff.py
@@ -5,13 +5,15 @@ from .utils.dataIO import dataIO
 import random
 rand=random.randint
 factrue = True
-
+class stuff():
+  def __init__(self, bot):
+    self.bot=bot
 @self.command(pass_context=True)  
-async def marco(ctx):
+async def marco(self, ctx):
   await self.say("POLO!")
 
 @self.command(pass_context=True)
-async def personinfo(ctx, user: discord.Member):
+async def personinfo(self, ctx, user: discord.Member):
   embed=discord.Embed(title="User {}'s info".format(user.name), description = "Here's what I found!", color=0x1971ff)
   embeded.add_field(name="Name", value= user.name, inline=True)
   embeded.add_field(name="ID", vlaue= user.id, inline=True)
@@ -20,24 +22,24 @@ async def personinfo(ctx, user: discord.Member):
   embeded.add_field(name="Lowest Role",value=user.role_bottom,inline=True)
   embeded.add_field(name="Joined",value=user.joined_at,inline=True)
   embeded.set_thumbnail(url=user.avatar_url)
-  await self.say(embeded = embeded)
+  await self.bot.say(embeded = embeded)
   
 @self.command(pass_context=True)
-async def severinfo(ctx)
+async def severinfo(self, ctx)
 embeded=discord.Embeded(title="Server {}'s info".format(ctx.message.server.name), description="Here's what I found!", color=0xff8d3d)
 embeded.add_field(name="Server Name:", value = ctx.message.server.name, inline=True)
 embeded.add_field(name="Server ID:", value = ctx.message.server.id, inline = True)
 embeded.add_field(name="Companies:", value = ctx.len(message.server.roles), inline=True)
 embeded.add_field(name="Members:", value = ctx.len(message.server.members), inline=True)
 embeded.set_thumbnail(url=ctx.server.icon_url)
-await self.say(embeded= embeded)
+await self.bot.say(embeded= embeded)
 
 @self.command(pass_context=True)
-async def randomworldfacts(ctx):
+async def randomworldfacts(self, ctx):
   world=["In Japan, you can hire a handsome man to watch sappy movies with you and wipe away your tears.", "In China, any TV shows and films featuring time travel are censored.", " Ethiopia follows a calendar that is seven years behind the rest of the world.","In Denmark, citizens have to select baby names from a list of 7,000 government-approved names.", "In Singapore, selling, importing or spitting out chewing gum is illegal.", "In France, you can marry a dead person.", "In Niue, an island nation in the South Pacific, its coins feature Disney and “Star Wars” characters.", "In Russia, authorities proposed to ban all things “emo” because they were declared a threat to national stability.",]
   while factrue == True:
     time.sleep(120)
-    await self.say(world[0,len(world)-1])
+    await self.bot.say(world[0,len(world)-1])
   
 
   
